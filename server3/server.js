cb5240716ec3ae11aee81b053654a5b073359b28
NILTON CESAR <nilton.jacintho@gmail.com>
2018-09-10 22:50:03 -0300

    inicio ainda

--- server3/server.js
+++ server3/server.js
@@ -25,6 +25,17 @@ server.use(restify.plugins.bodyParser());
 
 // queueRouter.applyRoutes(server, '/queue/');
 
+const corsMiddleware = require('restify-cors-middleware')
+
+const cors = corsMiddleware({
+  preflightMaxAge: 5, //Optional
+  origins: ['http://api.myapp.com', 'http://web.myapp.com'],
+  allowHeaders: ['API-Token'],
+  exposeHeaders: ['API-Token-Expiry']
+})
+
+server.pre(cors.preflight)
+server.use(cors.actual)
 
 server.listen(port, function () {
   util.inicializarConfiguracao();
