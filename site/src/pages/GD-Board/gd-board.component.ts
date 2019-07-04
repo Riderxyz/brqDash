8984bc773afe22c806cb48cf23d6a055de975a3a
Unknown <ifavilla@gmail.com>
2019-07-04 00:58:47 -0300

    GD em 40%

--- site/src/pages/GD-Board/gd-board.component.ts
+++ site/src/pages/GD-Board/gd-board.component.ts
@@ -28,39 +28,91 @@ export class GDBoardComponent implements OnInit {
     this.gridOptions = {
       columnDefs: this.createColumnDefs,
       enableSorting: true,
-      headerHeight: 0,
       rowHeight: 100,
       onGridReady: (params) => {
         this.gridApi = params.api;
         params.api.sizeColumnsToFit();
         console.log('esta pronto');
-this.gridOptions.api.setRowData(this.esteiras)
-/*         const comando = config.rxjsCentralKeys.GridReady;
-        this.centralRx.sendData = comando;
-        this.isGridReady = true; */
+        this.gridOptions.api.setRowData(this.esteiras);
+        /*         const comando = config.rxjsCentralKeys.GridReady;
+                this.centralRx.sendData = comando;
+                this.isGridReady = true; */
       }
+    };
   }
-}
 
 
 
-get createColumnDefs(): Array<any> {
-  const that = this;
-  return this.columnDefs = [
-    {
-      headerName: 'Esteira',
-      field: 'esteira',
-      width: 80,
-      // height: 190,
-      cellRenderer: this.formatTest,
-    }
-  ];
-}
+  setarGrid() {
+    this.gridOptions.api.setRowData(this.esteiras);
+  }
+
+  get createColumnDefs(): Array<any> {
+    const that = this;
+    return this.columnDefs = [
+      {
+        headerName: 'Esteira',
+        field: 'esteira',
+        width: 80,
+        // height: 190,
+        cellRenderer: this.formatNome,
+      },
+      {
+        headerName: 'Horas Entregues',
+        field: 'esteira',
+        width: 80,
+        // height: 190,
+        cellRenderer: this.formatHoraEntregues,
+      },
+      {
+        headerName: 'Horas Previstas',
+        field: 'esteira',
+        width: 80,
+        // height: 190,
+        cellRenderer: this.formatHoraPrevista,
+      },
+      {
+        headerName: 'Meta Cumprida',
+        field: 'esteira',
+        width: 80,
+        // height: 190,
+        cellRenderer: this.formatMetaCumprida,
+      },
+      {
+        headerName: 'Produção Mensal',
+        field: 'esteira',
+        width: 80,
+        // height: 190,
+        cellRenderer: this.formatProducaoMensal,
+      }
+    ];
+  }
 
 
 
-formatTest(param: any) {
-  console.log(param.data);
-  return param;
-}
+  formatNome(param: any) {
+    const dados: GDInterface = param.data;
+    const html = '<br><span class="ColunaStatus">' + dados.nome + '</span>';
+    return html;
+  }
+  formatHoraEntregues(param: any) {
+    const dados: GDInterface = param.data;
+    const html = '<br><span class="ColunaStatus">' + dados.dados.metas.horasEntregues + '</span>';
+    return html;
+  }
+  formatHoraPrevista(param: any) {
+    const dados: GDInterface = param.data;
+    const html = '<br><span class="ColunaStatus">' + dados.dados.metas.horasPrev + '</span>';
+    return html;
+  }
+  formatMetaCumprida(param: any) {
+    const dados: GDInterface = param.data;
+    const html = '<br><span class="ColunaStatus">' + dados.dados.metas.metaCumprida + '</span>';
+    return html;
+  }
+  formatProducaoMensal(param: any) {
+    const dados: GDInterface = param.data;
+    const html = '<br><span class="ColunaStatus">' + dados.dados.metas.prodMensal + '</span>';
+    return html;
+  }
 }
